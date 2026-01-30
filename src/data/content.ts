6c85883610f9db4f89ff78cc17c5143bbe59c3ac
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2026-01-30 18:36:26 +0200

    Update collection entries in content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -72,19 +72,19 @@ export const siteContent = {
     enabled: true,
     collections: [
       {
-        url: "https://objkt.com/collections/KT1KS9HczgmgFuqkSSe3AeZsbu7eyH9MeRXZ",
-        name: "Cosmic Inspirations: Digital Echoes",
-        image: "/Cosmic Cube Realm.png"
-      },
-      {
         url: "https://opensea.io/collection/cosmic-inspirations-animated-edition",
         name: "Cosmic Inspirations: Animated Edition",
         image: "/Animated Edition In the Spirit of Gustav Klimt.png" 
       },
       {
+        url: "https://objkt.com/collections/KT1KS9HczgmgFuqkSSe3AeZsbu7eyH9MeRXZ",
+        name: "Cosmic Inspirations: Digital Echoes",
+        image: "/Cosmic Cube Realm.png"
+      },
+      {
         url: "https://opensea.io/collection/cosmic-inspirations-master-study-sketches",
         name: "Cosmic Inspirations: Retro Edition",
-        image: "/Cosmic Inspirations by AlphaRoss in the Style Alexander Golovin.png"
+        image: "/Cosmic Inspirations by AlphaRoss in the Style Alexander Golovin.jpeg"
       },
       {
         url: "https://opensea.io/collection/alphaross-season-1-cosmic-inspirations",
