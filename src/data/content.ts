1e30f76c7b43585dbb87fbf8ac27625d340f0102
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2026-01-30 21:55:11 +0200

    Update content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -28,9 +28,9 @@ export interface SiteContent {
 export const siteContent: SiteContent = {
   // Твои новые достижения
   winnerShowcase: {
-    title: "International Open Call Selection",
+   title: "International Open Call Selection",
     subtitle: "Exhibited at Art Basel Miami & Global Digital Galleries",
-    description: "Selected Artist: Curated by 34 Gallery, an organization promoting creative arts advocacy in alignment with UN SDG 3.4. Miami Art Week: Exhibited in One Love Miami (event featured by Forbes). Recognition: Contributor to #FCancer2025 Art for Impact. More in the About Me section",
+    description: "Selected Artist: Curated by 34 Gallery, an organization promoting creative arts advocacy in alignment with UN SDG 3.4. Miami Art Week: Exhibited in One Love Miami (event featured by Forbes). Recognition: Contributor to #FCancer2025 Art for Impact. More in the About Me section.",
     video: "https://www.youtube.com/embed/XHV8HmJsqyQ", 
     enabled: true,
     gallery: [
